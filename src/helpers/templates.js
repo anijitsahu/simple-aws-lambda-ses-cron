ab5f8e702435cdb22018a0f685cfd73a0bf8bef4
anijit <anijitsau@gmail.com>
2022-06-14 18:23:11 +0530

    templates, helper functions are added

---
+++ src/helpers/templates.js
@@ -0,0 +1,5 @@
+const sampleEmailTemplate = {
+  emailContent: "<h4>Another Sample EMail</h4>",
+};
+
+export { sampleEmailTemplate };
--- src/sendEmail.js
+++ src/sendEmail.js
@@ -1,48 +1,51 @@
-import {
-  ListIdentitiesCommand,
-  VerifyEmailIdentityCommand,
-  SendEmailCommand,
-} from "@aws-sdk/client-ses";
+// AWS SDK dependencies
+import { SendEmailCommand } from "@aws-sdk/client-ses";
+
+// local dependencies
 import { emailClient } from "./libs/emailClient.js";
+import { listGenericIdentities } from "./helpers/listGenericEmailIdentities.js";
+import { sampleEmailTemplate } from "./helpers/templates.js";
 
-export async function sendEmailHandler(event) {
+export async function sendEmailHandler() {
   try {
-    const params = {
-      IdentityType: "EmailAddress",
-      MaxRecords: 10,
-    };
+    // list set of Generic Email Accounts which will send the emails
+    const genericAccounts = await listGenericIdentities();
 
-    const { Identities } = await emailClient.send(
-      new ListIdentitiesCommand(params)
-    );
-
-    const emailParams = {
-      Destination: {
-        CcAddresses: [],
-        ToAddresses: ["anijitsau@gmail.com"],
-      },
-      Message: {
-        Body: {
-          Html: {
+    if (Array.isArray(genericAccounts)) {
+      const emailParams = {
+        Destination: {
+          CcAddresses: [],
+          // list of receiver accounts
+          ToAddresses: ["anijitsau@gmail.com"],
+        },
+        Message: {
+          Body: {
+            Html: {
+              Charset: "UTF-8",
+              Data: sampleEmailTemplate.emailContent,
+            },
+          },
+          Subject: {
             Charset: "UTF-8",
-            Data: "<h3>A sample Email from AWS",
+            Data: process.env.SAMPLE_EMAIL_SUBJECT,
           },
         },
-        Subject: {
-          Charset: "UTF-8",
-          Data: "Sample Email",
-        },
-      },
-      Source: Identities[0], // SENDER_ADDRESS
-      ReplyToAddresses: [],
-    };
+        Source: genericAccounts[0],
+        ReplyToAddresses: [],
+      };
 
-    const emailData = await emailClient.send(new SendEmailCommand(emailParams));
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ Identities, emailData }),
-    };
+      // sending the Email
+      const emailData = await emailClient.send(
+        new SendEmailCommand(emailParams)
+      );
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          genericAccounts,
+          sendEmailCode: emailData["$metadata"].httpStatusCode,
+        }),
+      };
+    }
   } catch (error) {
     return {
       statusCode: 400,
